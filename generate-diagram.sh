0ad3fb609f065b3a797179a424c447d6b1e6658d
Ksenia Kotelnikova <xeniia.ka@gmail.com>
2025-12-07 22:30:45 +0300

    feat: add serch for get requests into main script
    Signed-off-by: Ksenia Kotelnikova <xeniia.ka@gmail.com>

--- generate-diagram.sh
+++ generate-diagram.sh
@@ -22,6 +22,11 @@ codeql bqrs decode diag-data/$PROJECT_NAME/discovery-server.bqrs   --format=csv 
 codeql query run codeql-query/eureka-load-balanced.ql   --database codeql-dbs/codeql-db-petclinic-microservices   --output diag-data/$PROJECT_NAME/eureka-load-balanced.bqrs
 codeql bqrs decode diag-data/$PROJECT_NAME/eureka-load-balanced.bqrs   --format=csv   --output diag-data/$PROJECT_NAME/eureka-load-balanced.csv
 
+# searching for REST requesters
+codeql query run codeql-query/rest-requesters.ql   --database codeql-dbs/codeql-db-petclinic-microservices   --output diag-data/$PROJECT_NAME/rest-requesters.bqrs
+codeql bqrs decode diag-data/$PROJECT_NAME/rest-requesters.bqrs   --format=csv   --output diag-data/$PROJECT_NAME/rest-requesters.csv
+
+# searching for app names in Eureka (for mapping them with REST requests later)
 ./find-yml-app-configs.sh $PROJECT_DIR diag-data/$PROJECT_NAME
 
 echo "Building diagram"
